5039124f3969ed954e2f9f1c8b81ed500e5256e6
runnerhuang <45131712@qq.com>
2020-04-10 17:11:44 +0800

    init project

---
+++ src/test/java/com/test/jacoco/test/HelloWorldTest.java
@@ -0,0 +1,34 @@
+package com.test.jacoco.test;
+
+/**
+ * Created by zc on 2017/5/2.
+ */
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import org.junit.Test;
+import com.test.jacoco.HelloWorld;
+
+public class HelloWorldTest {
+    @Test
+    public void testMethod1() {
+        HelloWorld hw = new HelloWorld();
+        String ss = hw.testMethod1();
+        TestCase.assertNotNull(ss);
+    }
+
+    @Test
+    public void testMethod2() {
+        HelloWorld hw = new HelloWorld();
+        int ss = hw.addMethod(1, 1);
+        TestCase.assertEquals(ss, 2);
+    }
+
+    @Test
+    public void testMethod3(){
+        HelloWorld hw = new HelloWorld();
+        int cc = hw.addMethod(3,2);
+        TestCase.assertEquals(cc, 5);
+
+    }
+}
